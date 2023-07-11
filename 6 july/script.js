9d70245d3875f83ea52e3cacd52665367355ce18
Zhiide Dzhuzumkulova <zhiidedzhuzumkulova@Zhiides-iMac.attlocal.net>
2023-07-11 12:04:30 -0400

    final commit

--- 6 july/script.js
+++ 6 july/script.js
@@ -1,4 +1,23 @@
 
 console.log('Hello World')
 
+//const firstName = 'GIGI'
+//const town = 'Hallandale Beach'
+//const population =  2000000 
+//console.log('Hello my name is ' + firstName + 'i live in'  + town + ' population of this city is ' + population )
 
+//const r = 12
+//const b = 34
+//const c = 14
+//console.log(`rgb(${r},${b},${c})`)
+//console.log(result)
+
+//console.log( 'b' + 'a' +  + 'c')
+
+//const firstRequest = prompt ('Enter your age')
+ //console.log(firstRequest*2)
+
+
+const a = prompt('firts number')
+const ba = prompt('second number')
+console.log( a + ba)
